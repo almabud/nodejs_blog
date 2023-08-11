55928416848db092eb641fa5a54231795e81d161
Almabud Juwal <almabud37@gmail.com>
2023-08-11 18:08:24 +0600

    - Implement get() method in repo.

--- src/repositories/base_repo.ts
+++ src/repositories/base_repo.ts
@@ -2,7 +2,7 @@ export interface BaseRepo<T>{
     model: T | any
 
     filter(filters?: Object): Promise<T[]>;
-    // get(filters?: Object): Promise<T>;
+    get(filters: {[key: string]: any}): Promise<T>;
     get_by_id(id: string): Promise<T>;
     create(data: T): Promise<T>;
 }
