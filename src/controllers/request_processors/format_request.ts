42dce1e7b877473db72046794e24fc7e325ce5ba
Almabud Juwal <almabud37@gmail.com>
2023-08-14 14:50:45 +0600

    Update format_request.ts

--- src/controllers/request_processors/format_request.ts
+++ src/controllers/request_processors/format_request.ts
@@ -1,12 +1,16 @@
 import { BaseRequestProcessor } from './base_request_processor';
-import {BaseRequest} from '../../entity/request'
+import { BaseRequest } from '../../entity/request'
 
 
 /**
  * This request processor should be run at the begenning of all request processors.
  */
-export class FormatRequest implements BaseRequestProcessor{
-    async process(request: Request | BaseRequest): Promise<BaseRequest>{
-        return new BaseRequest().deserialize(request)
+export class FormatRequest implements BaseRequestProcessor {
+    async process(request: { [key: string]: any } | BaseRequest): Promise<BaseRequest> {
+        return new BaseRequest().deserialize(Object.assign(
+            {},
+            { 'headers': request.headers, 'data': request.body },
+            request
+        ))
     }
 }
